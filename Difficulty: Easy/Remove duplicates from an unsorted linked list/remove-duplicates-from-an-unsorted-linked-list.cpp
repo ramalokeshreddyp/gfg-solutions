f742e5460b15cfc6e7cfdb71e3d8a3a83948bfb0
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-09-29 08:58:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x)
+        : data(x)
+        , next(nullptr) {}
+};
+
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        // your code goes here
+        set<int>s;
+        Node *temp=head,*prev=NULL,*del=NULL;
+        while(temp!=NULL){
+            if(s.find(temp->data)!=s.end()){
+              prev->next=temp->next;
+              del=temp;
+              temp=temp->next;
+              delete del;
+            }
+            else{
+            s.insert(temp->data);
+            prev=temp;
+            temp=temp->next;
+        }
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << "empty" << endl;
+            continue;
+        }
+
+        Node *head = new Node(arr[0]);
+        Node *tail = head;
+
+        for (size_t i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *result = ob.removeDuplicates(head);
+        printList(result);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
