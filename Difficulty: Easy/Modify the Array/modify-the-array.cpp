cb279a4ff2e17a8b9503ea5847c8a10a0d22fef7
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-10-18 05:52:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Modify the Array/modify-the-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) {
+        // Complete the function
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=0&&arr[i]==arr[i+1]){
+                arr[i]=2*arr[i];
+                arr[i+1]=0;
+                i++;
+            }
+        }
+        stable_partition(arr.begin(),arr.end(),[](int num){return num!=0;});
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.modifyAndRearrangeArray(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
