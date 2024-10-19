9c4ce48f7924c04da2f835c7259c61632f2b26c3
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-10-19 20:31:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find n/k th node in Linked list/find-nk-th-node-in-linked-list.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+class Solution {
+  public:
+    int fractional_node(struct Node *head, int k) {
+        // your code here
+       
+        int n = 0;
+        struct Node *temp = head;
+        while (temp != NULL) {
+            n++;
+            temp = temp->next;
+        }
+
+      
+        int pos = ceil((double)n / k);
+
+       
+        temp = head;
+        for (int i = 1; i < pos; i++) {
+            temp = temp->next;
+        }
+
+        
+        return temp->data;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        int res = ob.fractional_node(head, k);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
