be084e49ff5f0066d616cc7ac1f3df5793cc7fad
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-10-27 12:57:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 15/pattern-15.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            char c='A';
+            for(int j=i;j<=n;j++){
+                cout<<c;
+                c++;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
