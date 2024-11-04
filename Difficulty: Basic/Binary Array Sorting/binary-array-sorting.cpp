24207d387a15140cc48b81162797d85328a6e69d
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-11-04 19:34:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Array Sorting/binary-array-sorting.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void binSort(int n, vector<int> &arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        obj.binSort(a.size(), a);
+
+        for (int i = 0; i < a.size(); i++) {
+            if (i != 0)
+                cout << " ";
+            cout << a[i];
+        }
+        cout << endl;        // Ensure new line after each test case output
+        cout << "~" << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
