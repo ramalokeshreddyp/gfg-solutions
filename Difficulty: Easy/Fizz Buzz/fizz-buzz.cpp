874f782467a260c344bdf98323d2abf4f5bea2ba
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-11-27 08:54:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Fizz Buzz/fizz-buzz.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            cin >> A[i];
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<string> fizzBuzz(int n) {
+        // code here
+        vector<string>res;
+        for(int i=1;i<=n;i++){
+            if(i%3==0&&i%5==0){
+                res.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                res.push_back("Fizz");
+            }
+            else if(i%5==0){
+                res.push_back("Buzz");
+            }
+            else{
+                 res.push_back(to_string(i));
+            }
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        vector<string> res = obj.fizzBuzz(n);
+
+        Array::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
