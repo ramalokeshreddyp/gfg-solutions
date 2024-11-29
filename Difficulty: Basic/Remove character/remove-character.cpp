0fe68abfa787726ba1ff7caba314503efa1043a4
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-11-29 13:49:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove character/remove-character.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    string removeChars(string string1, string string2) {
+        // code here
+        for(int i=0;string2[i]!='\0';i++){
+            auto new_end=remove(string1.begin(),string1.end(),string2[i]);
+            string1.erase(new_end,string1.end());
+        }
+        return string1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string string1,string2;
+        cin >> string1; 
+        cin >> string2;
+        Solution ob;
+        cout << ob.removeChars(string1,string2) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
