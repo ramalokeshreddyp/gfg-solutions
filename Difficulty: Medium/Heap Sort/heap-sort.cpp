79b3d49e53b04a18e7a32e80999c79466a7a7978
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-11-29 07:51:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Heap Sort/heap-sort.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted
+// in increasing order when heapSort() is called.
+
+class Solution {
+  public:
+    // Function to sort an array using Heap Sort.
+    void heapSort(vector<int>& arr) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.end());
+        arr.clear();
+        while(!pq.empty()){
+            arr.push_back(pq.top());
+            pq.pop();
+        }
+      
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        obj.heapSort(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        // cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
