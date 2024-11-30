8e9ecf40c6b2fe8cdd823bc2d6a63d63c4676aa9
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2024-11-30 10:48:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> lcmAndGcd(int a, int b) {
+        // code here
+    int m=a,n=b;
+    while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    int lcm=(m*n)/a;
+    return {lcm,a};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int A, B;
+
+        cin >> A >> B;
+
+        Solution ob;
+        vector<int> ans = ob.lcmAndGcd(A, B);
+        cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
