d963b026daf5c236192e8ee53db31095a6f6218b
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-03-16 07:17:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Space/print-without-space.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    getline(cin, a);
+    string b;
+    getline(cin, b);
+    // Print in the required format and a new line
+    cout<<a<<b<<endl;
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
