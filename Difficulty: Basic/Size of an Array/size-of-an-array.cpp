5f815da7aa3a7443bfff8399ba2c3903de5ff982
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-04-17 06:15:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of an Array/size-of-an-array.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        
+// } Driver Code Ends
+
+// User function Template for C++
+
+// arr[] : declared and initialized above
+
+// calculate the size of the array arr[]
+int size =sizeof(arr)/sizeof(arr[0]) ;
+cout << size << endl;
+// driver code prints size
+
+
+//{ Driver Code Starts.
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
