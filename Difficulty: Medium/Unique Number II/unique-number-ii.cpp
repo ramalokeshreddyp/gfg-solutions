2855d8b3c004e364d5e2c2efde402b9326ce9d5a
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-04-23 06:06:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Number II/unique-number-ii.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> singleNum(vector<int>& arr) {
+    int xorAll = 0;
+    for (int num : arr) {
+        xorAll ^= num;
+    }
+
+    int rightmostBit = xorAll & -xorAll;
+
+    int x = 0, y = 0;
+    for (int num : arr) {
+        if (num & rightmostBit)
+            x ^= num;
+        else
+            y ^= num;
+    }
+
+    if (x > y) swap(x, y);
+    return {x, y};
+}
+
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.singleNum(arr);
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
