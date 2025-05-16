822e7e136019124d3a27b981c70d7f155f1f3255
ramalokeshreddyp <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-05-16 09:18:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Statement/print-statement.java
@@ -0,0 +1,18 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        // Print S and move the cursor to a new line
+        System.out.println(s);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
