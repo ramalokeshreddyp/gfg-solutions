a067acefbd11c21869c64166eed97ac012abe6ab
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-02-25 08:49:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.cpp
@@ -1,52 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
 class Solution {
   public:
     int longestSubarray(vector<int> &arr, int k) {
         // Code here
- int c=0,ans=0;
-        unordered_map<int,int>m;
-        for(int i=0;i<arr.size();i++) {
-            if(arr[i]<=k) c--;
-            else c++;
-            if(c<=0 and !m.count(c)) m[c]=i;
-            if(c<=0 and m.count(c-1))ans=max(ans,i-m[c-1]);
-            if(c>0) ans=i+1;
+          int n = arr.size();
+        unordered_map<int, int> firstIndex;
+        int sum = 0, maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += (arr[i] > k) ? 1 : -1;
+
+            if (sum > 0)
+                maxLen = i + 1;
+
+            if (firstIndex.find(sum) == firstIndex.end())
+                firstIndex[sum] = i;
+
+            if (firstIndex.find(sum - 1) != firstIndex.end())
+                maxLen = max(maxLen, i - firstIndex[sum - 1]);
         }
-        return ans;
+
+        return maxLen;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        int k = stoi(input);
-
-        Solution ob;
-        cout << ob.longestSubarray(arr, k) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
