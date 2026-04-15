ee4acf7982c9108983fa6996fd71f51ef9e0eae6
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-04-15 05:55:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    string URLify(string &s) {
+        string result = "";
+        
+        for (char c : s) {
+            if (c == ' ')
+                result += "%20";
+            else
+                result += c;
+        }
+        
+        return result;
+    }
+};
