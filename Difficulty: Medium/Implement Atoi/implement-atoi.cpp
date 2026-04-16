2a7fd0df214480730e505b6297d1ad4ec1fac9fd
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-04-16 14:49:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.cpp
+++ Difficulty: Medium/Implement Atoi/implement-atoi.cpp
@@ -1,52 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int myAtoi(char *s) {
-        // Your code here
-        int i = 0, sign = 1; 
-    long result = 0;     
-  
-    while (s[i] == ' ') {
-        i++;
-    }
-
-    if (s[i] == '-' || s[i] == '+') {
-        sign = (s[i] == '-') ? -1 : 1;
-        i++;
-    }
-
-    while (s[i] >= '0' && s[i] <= '9') {
-        result = result * 10 + (s[i] - '0');
-
-      
-        if (result * sign > INT_MAX) {
-            return INT_MAX;
+    int myAtoi(string &s) {
+        int i = 0, n = s.size();
+        while (i < n && s[i] == ' ') i++;
+        
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
         }
-        if (result * sign < INT_MIN) {
-            return INT_MIN;
+        
+        long long res = 0;
+        while (i < n && s[i] >= '0' && s[i] <= '9') {
+            res = res * 10 + (s[i] - '0');
+            if (sign * res > INT_MAX) return INT_MAX;
+            if (sign * res < INT_MIN) return INT_MIN;
+            i++;
         }
-        i++;
-    }
-    return (int)(result * sign);
+        
+        return (int)(sign * res);
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        char s[20];
-        cin >> s;
-        Solution ob;
-        int ans = ob.myAtoi(s);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-}
-// } Driver Code Ends
