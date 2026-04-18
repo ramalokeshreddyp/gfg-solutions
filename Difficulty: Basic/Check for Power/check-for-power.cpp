762f8e1b6df97ccb7e93fbec8401dd2b69dd3300
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-04-19 04:59:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Power/check-for-power.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool isPower(int x, int y) {
+      
+        if (y == 1) return true;
+        if (x == 1) return false;
+
+        while (y % x == 0) {
+            y /= x;
+        }
+
+        return (y == 1);
+    }
+};
