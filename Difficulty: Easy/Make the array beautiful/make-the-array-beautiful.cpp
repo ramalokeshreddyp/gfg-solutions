628360597aafbb816527d9301a2851094d980ab6
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-05-17 17:28:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        vector<int> st;
+        
+        for (int x : arr) {
+            if (!st.empty() && ((st.back() >= 0 && x < 0) || (st.back() < 0 && x >= 0))) {
+                st.pop_back();
+            } else {
+                st.push_back(x);
+            }
+        }
+        
+        return st;
+    }
+};
