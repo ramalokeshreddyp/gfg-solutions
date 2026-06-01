c68c23aa19e341bdb741249e5bf3cf072ac9fd17
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-06-01 12:29:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
@@ -1,60 +1,43 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int findSubString(string& str) {
-    int n = str.length();
-    if (n == 0) return 0;
+        // code here
+         string ans;
 
-    unordered_set<char> unique_chars(str.begin(), str.end());
-    int total_unique = unique_chars.size();
-
-    unordered_map<char, int> window_freq;
-    int min_len = n + 1;
-    int start = 0, count = 0;
-
-    for (int end = 0; end < n; ++end) {
-        char end_char = str[end];
-        window_freq[end_char]++;
-        if (window_freq[end_char] == 1)
-            count++;
-
-        while (count == total_unique) {
-            min_len = min(min_len, end - start + 1);
-            char start_char = str[start];
-            window_freq[start_char]--;
-            if (window_freq[start_char] == 0)
-                count--;
-            start++;
-        }
-    }
-
-    return min_len;
-
+ int hash[26]={0};
+ for(auto it:str){
+     if(hash[it-'a']==1){
+         continue;
+     }
+     hash[it-'a']=1;
+     ans+=it;
+ }
+ 
+ int n=str.size(),m=ans.size(),l=0,r=0,minl=1e9,cnt=0;
+ int  has[256]={0};
+ for(int i=0;i<m;i++){
+     has[ans[i]]++;
+ }
+ while(r<n){
+     if(has[str[r]]>0){
+         cnt+=1;;
+     }
+         has[str[r]]--;
+    
+     while(cnt==m){
+         if(r-l+1<minl){
+             minl=r-l+1;
+         }
+         has[str[l]]++;
+         if(has[str[l]]>0){
+             cnt-=1;
+         }
+         l++;
+     }
+     r++;
+ }
+ 
+ return minl;
+ 
     }
 };
-
-
-//{ Driver Code Starts.
-//      Driver code
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-
-        string str;
-        cin >> str;
-        Solution ob;
-        cout << ob.findSubString(str) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
