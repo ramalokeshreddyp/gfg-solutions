25980db3ec00a5dbe6dfeb2efeb41afe91ec5545
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-06-03 05:31:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -1,45 +1,31 @@
 class Solution {
-public:
-    string minWindow(string &s, string &t) {
-
-        vector<int> need(26, 0);
-
-        int required = t.size();
-
-        for(char c : t)
-            need[c - 'a']++;
-
-        int l = 0, r = 0;
-        int formed = 0;
-
-        int bestLen = INT_MAX;
-        int bestStart = -1;
-
-        while(r < s.size()) {
-
-            if(need[s[r] - 'a'] > 0)
-                formed++;
-
-            need[s[r] - 'a']--;
-
-            while(formed == required) {
-
-                if(r - l + 1 < bestLen) {
-                    bestLen = r - l + 1;
-                    bestStart = l;
-                }
-
-                need[s[l] - 'a']++;
-
-                if(need[s[l] - 'a'] > 0)
-                    formed--;
-
-                l++;
+  public:
+    string minWindow(string &s, string &p) {
+        // code here
+        int n=s.size(),m=p.size(),ml=1e9,si=-1;
+        int hash[256]={0},l=0,r=0;
+        for(int i=0;i<m;i++){
+            hash[p[i]]++;
+        }
+        int cnt=0;
+        while(r<n){
+            if(hash[s[r]]>0){
+                cnt++;
             }
-
+            hash[s[r]]--;
+            while(cnt==m){
+                if(r-l+1<ml){
+                    ml=r-l+1;
+                    si=l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]]>0){
+                    cnt--;
+                }
+            l++;    
+            }
             r++;
         }
-
-        return (bestStart == -1) ? "" : s.substr(bestStart, bestLen);
+    return (si == -1) ? "" : s.substr(si, ml);
     }
 };
