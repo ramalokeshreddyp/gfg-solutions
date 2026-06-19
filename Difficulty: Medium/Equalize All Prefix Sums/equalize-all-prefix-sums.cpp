5e6f12cef40eda72804f6b74137b3f8daeb3c7b2
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-06-19 13:03:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Equalize All Prefix Sums/equalize-all-prefix-sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> optimalArray(vector<int>& arr) {
+        int n = arr.size();
+        vector<long long> pref(n);
+        pref[0] = arr[0];
+
+        for (int i = 1; i < n; i++)
+            pref[i] = pref[i - 1] + arr[i];
+
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            int m = i / 2;
+
+            long long left = 1LL * arr[m] * (m + 1) - pref[m];
+            long long right = (pref[i] - pref[m]) - 1LL * arr[m] * (i - m);
+
+            ans[i] = left + right;
+        }
+
+        return ans;
+    }
+};
