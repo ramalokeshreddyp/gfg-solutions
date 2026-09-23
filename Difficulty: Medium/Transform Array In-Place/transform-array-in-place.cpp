6b8bb19d91fb86538a920a8b3eded10475a163f0
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2026-09-23 18:19:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Transform Array In-Place/transform-array-in-place.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void arrange(vector<int>& arr) {
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] += (arr[arr[i]] % n) * n;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] /= n;
+        }
+    }
+};
